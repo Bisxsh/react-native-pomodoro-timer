9ce792024101c8deaea8fcf082a8bcd8e090ddba
Bisesh <bisesh.sitaula@gmail.com>
2022-08-04 16:04:39 +0100

    Created settings screen. Synced settings with App

--- App.tsx
+++ App.tsx
@@ -1,23 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import CircularProgress from "react-native-circular-progress-indicator";
+import { useState } from "react";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { AppSettings } from "./objects/Settings";
+import Settings from "./screens/Settings/Settings";
 import Timer from "./screens/Timer";
 
 export default function App() {
+  const [menuActive, setMenuActive] = useState(true);
+  const [appSetings, setAppSettings] = useState(new AppSettings());
+
+  function getMainView() {
+    if (!menuActive)
+      return (
+        <View style={[styles.container, styles.mainContainer]}>
+          <Timer />
+        </View>
+      );
+    return <Settings settings={appSetings} setSettings={setAppSettings} />;
+  }
+
+  function toggleMenu() {
+    setMenuActive(!menuActive);
+  }
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"></StatusBar>
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+          onPress={toggleMenu}
+          style={{ alignSelf: "flex-end" }}
+        >
+          <Image source={require("./assets/menu.png")} style={styles.menu} />
+        </TouchableOpacity>
 
-      <Timer />
+        {getMainView()}
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#4A4B4F",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+
+  menu: {
+    alignSelf: "flex-end",
+    marginRight: 20,
+    marginTop: 50,
   },
 });
